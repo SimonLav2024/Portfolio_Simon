5c17e02673a2d2c15154b4ec3f7ab9e6025086ba
SimonLav2024 <simonlav94@gmail.com>
2024-09-18 12:41:14 +0200

    añadido el sidebar pero todavia con errores

--- JavaScript/code.js
+++ JavaScript/code.js
@@ -8,3 +8,22 @@ const menuIcon = document.getElementById('menuIcon');
             menuIcon.classList.toggle('active');
         });
 //----------------------------------fin-----------------------------------
+
+// ------------------------------sidebar----------------------------------
+const menuBtn = document.querySelector('.menu-btn');
+const sidebar = document.querySelector('.sidebar');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        sidebar.classList.add('open');
+        menuOpen = true;
+        carrito.classList.remove("open");
+    } else {
+        menuBtn.classList.remove('open');
+        sidebar.classList.remove('open');
+        menuOpen = false;
+    }
+});
+// -------------------------------fin----------------------------------------
