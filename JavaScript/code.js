4218c99eb692142a6ffacdfeb0ed7cc96c3383d3
SimonLav2024 <simonlav94@gmail.com>
2024-09-19 12:23:48 +0200

    arreglado el boton del sidebar

--- JavaScript/code.js
+++ JavaScript/code.js
@@ -14,16 +14,15 @@ const menuBtn = document.querySelector('.menu-btn');
 const sidebar = document.querySelector('.sidebar');
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        sidebar.classList.add('open');
-        menuOpen = true;
-        carrito.classList.remove("open");
-    } else {
-        menuBtn.classList.remove('open');
-        sidebar.classList.remove('open');
-        menuOpen = false;
-    }
-});
+        menuBtn.addEventListener('click', () => {
+            if (!menuOpen) {
+                menuBtn.classList.add('open');
+                sidebar.classList.add('open');
+                menuOpen = true;
+            } else {
+                menuBtn.classList.remove('open');
+                sidebar.classList.remove('open');
+                menuOpen = false;
+            }
+        });
 // -------------------------------fin----------------------------------------
